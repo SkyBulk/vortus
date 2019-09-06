b2df4a98841f7af2861bdef3a598de7a1df91534
SkyBulk <43649816+SkyBulk@users.noreply.github.com>
2019-09-05 23:55:44 -0600

    Update vortus.py

--- vortus.py
+++ vortus.py
@@ -4,7 +4,7 @@ import optparse
 def main():
     parser = optparse.OptionParser()
     parser.add_option("-q", "--quiet", dest="quiet_mode_opt", action="store_true", default=False, help="Runs without displaying the banner.")
-    parser.add_option("-p", "--profile", dest="profile", help="Load weeman profile.")
+    parser.add_option("-p", "--profile", dest="profile", help="Load vortus profile.")
     options,r = parser.parse_args()
 
     if options.profile:
